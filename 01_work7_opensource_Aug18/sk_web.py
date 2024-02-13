43d7669e823df31ff35f62d813ff976b16ce2bd5
Shubham Kulkarni <skulkarni@mvista.com>
2024-02-13 18:12:53 +0530

    folder01 - python code creator from bugz data

---
+++ 01_work7_opensource_Aug18/sk_web.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+
+webbrowser.open('https://www.google.com/')
+webbrowser.open('https://git.openembedded.org/openembedded-core-contrib/log/?h=stable%2Fdunfell-nut&qt=grep&q=CVE-2021-31525')
+webbrowser.open('https://git.openembedded.org/openembedded-core-contrib/log/?h=stable%2Fdunfell-nut&qt=grep&q=CVE-2021-31525')
+webbrowser.open('https://git.openembedded.org/openembedded-core-contrib/log/?h=stable%2Fdunfell-nut&qt=grep&q=CVE-2021-31525')
