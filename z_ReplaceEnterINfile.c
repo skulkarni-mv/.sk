fc6cd2afd83f4d9225c125ca643bbb9c25455244
Shubham Kulkarni <skulkarni@mvista.com>
2024-02-13 17:59:10 +0530

    code for replacing the '\n' with '\0' in file

---
+++ z_ReplaceEnterINfile.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<string.h>
+
+// cmd from code : replaceEnter_sk
+
+void main(int argc, char *argv[])
+{
+	if(argc !=2)
+	{
+		printf("Invalid. Usage: replaceEnter_sk <file_name_to_replace_enter> \n");
+		return;
+	}
+	
+	char last_char=0;
+
+   	FILE *fp=fopen(argv[1],"r+");			// argv[1] = "home/shubham/.sk/bulkcp_dir_is.txt"
+ 
+        if(fp)                  // check if file exists
+        {
+        	fseek(fp,-1,SEEK_END);          	// Replacing Enter '\n' with space ' ' at EOF
+		if((last_char=fgetc(fp)) == '\n')
+		{
+        		fseek(fp,-1,SEEK_END);          // Replacing Enter '\n' with space ' ' at EOF
+                	fputc('\0',fp);
+		}
+		else
+			printf(" Enter NOT found at the end of File \n");
+
+                fclose(fp);
+ 	}
+}
+
