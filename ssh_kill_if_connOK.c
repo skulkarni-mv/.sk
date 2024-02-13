07c52f1ca2dbfb281d6b5dfc86edb82e8f4fae8c
Shubham Kulkarni <skulkarni@mvista.com>
2024-02-13 17:44:01 +0530

    SSHconTester & SSHkill binaries to check ssh connection with vm

---
+++ ssh_kill_if_connOK.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+void main()
+{
+	char buffer[20]={0}, str_pid[10]={0};
+
+	system("rm ~/.sk/ssh_kill.txt");
+	system("ps -e | grep SSHconTester > ~/.sk/ssh_kill.txt");
+
+	FILE *fp=fopen("/home/shubham/.sk/ssh_kill.txt", "r");			// Main program ID will be at TOP, so killing that will STOP all
+	if(fp != NULL)
+	{
+		fscanf(fp,"%s", str_pid);	
+		fclose(fp);
+
+		printf(" %s \n", str_pid);
+		
+		if(str_pid[0] == 0)	// NO PID to kill
+		{
+			printf("\n\n NO PID Found to be Killed. Exiting ... \n");
+
+			// -> Added extra by sk on Sep 14 2022
+			system("ps -e | grep ssh");
+			system("ps -e | grep ssh > ~/.sk/ssh_kill.txt");
+			fp=fopen("/home/shubham/.sk/ssh_kill.txt", "r");
+			
+			if(fp)
+			{
+        	        	fscanf(fp,"%s", str_pid);
+	        	        fclose(fp);
+				printf("killing pid ->  %s automatically at last...\n", str_pid);
+				
+				if(str_pid[0] != 0)
+				{
+					strcpy(buffer, "kill -9 ");
+		                        strcat(buffer, str_pid);
+					system(buffer);
+				}		
+			} // <- Added Extra by sk
+			
+			exit(0);
+		}
+			
+			strcpy(buffer, "kill -9 ");
+			strcat(buffer, str_pid);
+
+			printf("\t Killing pid %s ... \n", str_pid);
+			system(buffer);
+		
+		printf("\n Checking for Another Connection ...");
+		sleep(3);
+		main();
+	}
+	else
+		printf("\n fp = NULL. Unable to kill \n");
+
+}
---
+++ ssh_vm_trylogin.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+
+
+void main()
+{
+
+	static char otp_once=100;		// When VM restarts, it refuses connection, so give 2 attaempts for refuse
+
+	pid_t chl_id;
+	int delay = 5;
+
+	if( chl_id=fork() )	// Parent
+	{
+
+		otp_once--;	// reduce to 1 from 2 first time, then 0		
+
+		sleep(1);			// So that Child can complete first
+		printf("\r");			// Carriage Return
+		for(int i=0; i<15; i++)
+		{	
+			printf("."); fflush(stdout);
+			sleep(1);
+		}
+
+		system("ps -e | grep ssh > ssh_ps.txt");	// grep 'ssh' as vm strats session in ssh process
+
+		//sleep(1);
+		
+		FILE *fp=fopen("/home/shubham/.sk/ssh_ps.txt","r");
+
+		if(fp)			// File Read Successful
+		{
+			char buffer[20]={0}, str_kill[20]="kill -9 ";
+
+			//printf(" cat cmd __");	fflush(stdout);
+			system("cat /home/shubham/.sk/ssh_ps.txt");
+
+			if((fscanf(fp,"%s",buffer)) != EOF)
+			{					// pid of ssh read successfully
+			
+				strcat(str_kill, buffer);
+				printf("\r  found pid of SSH - %6s, %s . waiting for new connection in %d seconds \n", buffer, str_kill, delay);
+			
+				system(str_kill);
+				//sleep(delay);
+				for(int i=0; i<delay; i++)
+				{
+					printf("*");	fflush(stdout);
+					sleep(1);
+				}
+					printf("\n");
+				main();
+			}
+			else if(otp_once >= 0)
+			{
+				printf("\r Giving extra attempt, as otp_once = %d \n", otp_once);
+				main();
+			}
+			else
+			{
+				printf("\n\t No PID for SSH found. Exiting\n\n");
+				return;
+			}
+
+		}
+		else
+		{
+			printf("\n\t FILE not found. Exiting\n\n");
+			return;
+		}
+
+	}
+	else			// Child
+	{
+
+		char *space_found=NULL;
+		time_t t_sec;
+		struct tm tm;
+
+		t_sec = time(NULL);
+		tm =  *localtime(&t_sec);
+		
+		printf("\n\r");				// Carriage Return
+		printf("%2d:%2d:%2d", tm.tm_hour, tm.tm_min, tm.tm_sec);	fflush(stdout);
+		
+		printf(" child pid = %6d, opening new SSH connection: \n", getpid());		
+		system("ssh qauser@10.80.200.8");
+		exit(0);
+	}
+
+
+}
