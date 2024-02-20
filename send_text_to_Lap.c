0f0884969f51a082354e682cba4791565b74950d
Shubham Kulkarni <skulkarni@mvista.com>
2024-02-20 23:09:36 +0530

    sendtoLAP cmd Multiprocessing changes - in dev
    Added debug flag to reduce printed messages on terminal

--- send_text_to_Lap.c
+++ send_text_to_Lap.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 
 // Output cmd: /bin/sendtoLap
 // gcc ~/.sk/send_text_to_Lap.c -o ~/.sk/sendtoLAP; sudo mv ~/.sk/sendtoLAP /bin/
 
+//#define debug
 
 void main(int argc, char *argv[])
 {
@@ -30,7 +32,9 @@ void main(int argc, char *argv[])
 		printf("\n Received cmd: $ sendtoLap sendtoLap. Sending manually edited file ~/.sk/.sk_gitignore/send-THIS2Lap.txt \n");
 	}
 
+#ifdef debug
 	int mainpid = getpid();	printf(" main proc pid = %d \n\n", mainpid);
+#endif
 
 /////////////
 
@@ -44,10 +48,11 @@ void main(int argc, char *argv[])
 	if( (i++, p[0]=fork()) && (i++, p[1]=fork()) && (i++, p[2]=fork()) )
 	{
 		i=0;		// Parent
-		printf("in parent, p[0]=%d, p[1]=%d, p[2]=%d \n", p[0], p[1], p[2]);
-		printf("inside parent at start, pid=%d \n", getpid());
-//		sleep(2);
 
+		printf("in parent, p[0]=%d, p[1]=%d, p[2]=%d \n", p[0], p[1], p[2]);
+#ifdef debug
+		printf("inside parent at start, pid=%d \n", getpid());
+#endif
 		first_ret=wait(0);
 
 		for(int j=0; j<3; j++)
@@ -58,7 +63,9 @@ void main(int argc, char *argv[])
 			}
 			else
 			{
+#ifdef debug
 				printf(" should be killed pid is p[%d]=%d \n", j, p[j]);
+#endif
 				sprintf(kill_this_pid, "%d", p[j]);
 				strcpy(buffer_pid, "kill -9 ");
 				strcat(buffer_pid, kill_this_pid);
@@ -78,22 +85,34 @@ void main(int argc, char *argv[])
 		sleep(1);
 		printf("\n");
 		if(i==1)
-		{	//sleep(2);
+		{
+#ifdef debug
 			printf("inside child1 at start, pid=%d, ppid=%d \n", getpid(), getppid() );
-			system("ping -c 1 192.168.0.1 > /dev/null");
+#endif
+			system("ping -c 1 172.31.84.172 > /dev/null");
+#ifdef debug
 			printf("inside child1 at end.. \n");
+#endif
 		}
 		else if(i==2)
-		{	//sleep(4);
+		{
+#ifdef debug
 			printf("inside child2 at start, pid=%d, ppid=%d \n", getpid(), getppid() );
-			system("ping -c 1 0.0.0.0 > /dev/null");
+#endif
+			system("ping -c 1 192.168.50.134 > /dev/null");
+#ifdef debug
 			printf("inside child2 at end.. \n");
+#endif
 		}
 		else if(i==3)
-		{	//sleep(4);
+		{
+#ifdef debug
 			printf("inside child3 at start, pid=%d, ppid=%d \n", getpid(), getppid() );
-			system("ping -c 1 www.google.com > /dev/null");
+#endif
+			system("ping -c 1 10.80.10.3 > /dev/null");
+#ifdef debug
 			printf("inside child3 at end.. \n");
+#endif
 		}
 	}
 
@@ -101,13 +120,8 @@ void main(int argc, char *argv[])
 
 /////////////
 
-
-/*	strcpy(buffer, "sshpass -f /home/shubham/.sk/.localpass.txt scp /home/shubham/.sk/send-THIS2Lap.txt shubham@172.31.84.172:/home/shubham/.sk/send-ReceivedFromPC.txt");
-
-	system(buffer);
-	
-		strcpy(buffer, "sshpass -f /home/shubham/.sk/.localpass.txt scp /home/shubham/.sk/send-THIS2Lap.txt shubham@192.168.50.134:/home/shubham/.sk/send-ReceivedFromPC.txt");
-		system(buffer);
+/*
+"sshpass -f /home/shubham/.sk/.localpass.txt scp /home/shubham/.sk/send-THIS2Lap.txt shubham@xyz:/home/shubham/.sk/send-ReceivedFromPC.txt"
 	
 	printf("\n Use: send-cat / send-view (GEDIT)  to view the content sent from Laptop");
 	printf("\n Use: rec-cat  / rec-view  (GEDIT)  to view the content received from PC");
