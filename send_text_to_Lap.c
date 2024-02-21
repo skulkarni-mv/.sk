f1b96a6f0248c9cec3f6c2906dcd29cd585f39da
Shubham Kulkarni <skulkarni@mvista.com>
2024-02-21 19:21:16 +0530

    sendtoLap cmd - developed initial version

--- send_text_to_Lap.c
+++ send_text_to_Lap.c
@@ -35,10 +35,10 @@ void main(int argc, char *argv[])
 
 	//printf("size = %ld \n\n", sizeof(ip_matrix));
 
-	ip_matrix[1].matx_chproc_num = 1;  strcpy(ip_matrix[1].matx_ip,"8.8.8.4");   	   strcpy(ip_matrix[1].matx_nameof_ip,"Laptop-WiFi");
-	ip_matrix[2].matx_chproc_num = 2;  strcpy(ip_matrix[2].matx_ip,"192.168.50.134");  strcpy(ip_matrix[2].matx_nameof_ip,"Cisco_VPN");
-	ip_matrix[3].matx_chproc_num = 3;  strcpy(ip_matrix[3].matx_ip,"10.80.10.3");	   strcpy(ip_matrix[3].matx_nameof_ip,"NetExtender_VPN");
-	ip_matrix[4].matx_chproc_num = 4;  strcpy(ip_matrix[4].matx_ip,"111.111.111.111"); strcpy(ip_matrix[4].matx_nameof_ip,"Laptop_Ethernet");
+	ip_matrix[1].matx_chproc_num = 1;  strcpy(ip_matrix[1].matx_ip,"172.31.84.172");  strcpy(ip_matrix[1].matx_nameof_ip,"Office_WiFi");
+	ip_matrix[2].matx_chproc_num = 2;  strcpy(ip_matrix[2].matx_ip,"192.168.50.160"); strcpy(ip_matrix[2].matx_nameof_ip,"Cisco_VPN");
+	ip_matrix[3].matx_chproc_num = 3;  strcpy(ip_matrix[3].matx_ip,"10.80.10.30");	  strcpy(ip_matrix[3].matx_nameof_ip,"NetExtender_VPN");
+	ip_matrix[4].matx_chproc_num = 4;  strcpy(ip_matrix[4].matx_ip,"10.80.244.125");  strcpy(ip_matrix[4].matx_nameof_ip,"Office_Ethernet");
 
 	if(argc!=2)
 	{
@@ -77,7 +77,7 @@ void main(int argc, char *argv[])
 #endif
 		first_ret_pid=wait(0);
 
-		printf(" \t\t KEEP this pid=%d \n", first_ret_pid);
+//		printf(" \t\t KEEP this pid=%d \n", first_ret_pid);
 		for(int j=1; j<=ips_cnt; j++)
 		{
 			if(pid_arr[j] != first_ret_pid)	// KILL these PID
@@ -85,7 +85,7 @@ void main(int argc, char *argv[])
 				sprintf(pid_str, "%d", pid_arr[j]);
 				strcpy(buffer_pid_killer, "kill -9 ");	strcat(buffer_pid_killer, pid_str);
 
-				printf(" \t\t buffer = %s \n", buffer_pid_killer);
+				printf(" \t\t buffer = %s [%s]\n", buffer_pid_killer, ip_matrix[j].matx_nameof_ip);
 				system(buffer_pid_killer);
 			}
 			else
@@ -93,6 +93,7 @@ void main(int argc, char *argv[])
 				ret_chproc_num=j;
 			}
 		}
+		printf(" \t\t KEEP this pid=%d [%s]\n", first_ret_pid, ip_matrix[ret_chproc_num].matx_nameof_ip);
 
 		for(int j=1; j<=ips_cnt-1; j++)		// '-1' was done as already once wait(0) used to capture first_ret_pid
 		{
@@ -106,12 +107,12 @@ void main(int argc, char *argv[])
 		printf("\n KEEP child process number %d\n", ret_chproc_num);
 #endif
 
-		strcpy(buffer_scp_cmd, "sshpass -f /home/shubham/.sk/.sk_gitignore/.localpass.txt ");
+		strcpy(buffer_scp_cmd, "sshpass -v -f /home/shubham/.sk/.sk_gitignore/.localpass.txt ");
 		strcat(buffer_scp_cmd, "scp /home/shubham/.sk/.sk_gitignore/send-THIS2Lap.txt shubham@");
 		strcat(buffer_scp_cmd, ip_matrix[ret_chproc_num].matx_ip);
 		strcat(buffer_scp_cmd, ":/home/shubham/.sk/.sk_gitignore/send-ReceivedFromPC.txt");
 
-		printf("\n %s \n", buffer_scp_cmd);
+//		printf("\n %s \n", buffer_scp_cmd);
 		system(buffer_scp_cmd);
 
 			printf("\n Use: send-cat / send-view (GEDIT)  to view the content sent from Laptop");
@@ -125,7 +126,7 @@ void main(int argc, char *argv[])
 	}
 	else			      //		 10	   20   25   30     37
 	{			      //		 10	   20   25   30     37
-	sleep(1);		      //0123456789        0123456789012345678901234567 
+	//sleep(1);		      //0123456789        0123456789012345678901234567
 		strcpy(buffer_scp_cmd, "ping -c 1 ");	//xyz.xyz.xyz.xyz > /dev/null
 		//strcpy(buffer_scp_cmd+10, "xyz.xyz.xyz.xyz");
 		strcpy(buffer_scp_cmd+25, " > /dev/null");
@@ -149,19 +150,22 @@ void main(int argc, char *argv[])
 		{
 		case 1: strcpy(tmp_cp_buff, ip_matrix[1].matx_ip);	for(int j=strlen(tmp_cp_buff); j<23; j++){tmp_cp_buff[j]=' ';}
 			strncpy(buffer_scp_cmd+10, tmp_cp_buff, 15);	//printf("_%s_\n", buffer_scp_cmd);
-			system(buffer_scp_cmd);				break;
+			if( system(buffer_scp_cmd) != 0) {printf("\n\t Please check the IP Matrix.\t FAILED \t to connect any IP \n\n\n");} break;
 
 		case 2: strcpy(tmp_cp_buff, ip_matrix[2].matx_ip);	for(int j=strlen(tmp_cp_buff); j<23; j++){tmp_cp_buff[j]=' ';}
 			strncpy(buffer_scp_cmd+10, tmp_cp_buff, 15);  	//printf("_%s_\n", buffer_scp_cmd);
-			system(buffer_scp_cmd);				break;
+//			printf("%d\n",system(buffer_scp_cmd) );		break;
+			if( system(buffer_scp_cmd) != 0) {printf("\n\t Please check the IP Matrix.\t FAILED \t to connect any IP \n\n\n");} break;
 
 		case 3: strcpy(tmp_cp_buff, ip_matrix[3].matx_ip);	for(int j=strlen(tmp_cp_buff); j<23; j++){tmp_cp_buff[j]=' ';}
 			strncpy(buffer_scp_cmd+10, tmp_cp_buff, 15);  	//printf("_%s_\n", buffer_scp_cmd);
-			system(buffer_scp_cmd);				break;
+//			printf("%d\n",system(buffer_scp_cmd) );		break;
+			if( system(buffer_scp_cmd) != 0) {printf("\n\t Please check the IP Matrix.\t FAILED \t to connect any IP \n\n\n");} break;
 
 		case 4: strcpy(tmp_cp_buff, ip_matrix[4].matx_ip);	for(int j=strlen(tmp_cp_buff); j<23; j++){tmp_cp_buff[j]=' ';}
 			strncpy(buffer_scp_cmd+10, tmp_cp_buff, 15);  	//printf("_%s_\n", buffer_scp_cmd);
-			system(buffer_scp_cmd);				break;
+//			printf("%d\n",system(buffer_scp_cmd) );		break;
+			if( system(buffer_scp_cmd) != 0) {printf("\n\t Please check the IP Matrix.\t FAILED \t to connect any IP \n\n\n");} break;
 		}
 	}
 
