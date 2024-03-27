afe73495da0d55837e1e8e3573e97df8021fe206
Shubham Kulkarni <skulkarni@mvista.com>
2024-03-27 14:04:58 +0530

    sendtoLAP cmd - formatted code & updated comments

--- send_text_to_Lap.c
+++ send_text_to_Lap.c
@@ -39,7 +39,7 @@ void main(int argc, char *argv[])
 
 	ip_matrix[1].matx_chproc_num = 1;  strcpy(ip_matrix[1].matx_ip,"172.31.84.172");  strcpy(ip_matrix[1].matx_nameof_ip,"Office_WiFi");
 	ip_matrix[2].matx_chproc_num = 2;  strcpy(ip_matrix[2].matx_ip,"192.168.50.160"); strcpy(ip_matrix[2].matx_nameof_ip,"Cisco_VPN");
-	ip_matrix[3].matx_chproc_num = 3;  strcpy(ip_matrix[3].matx_ip,"10.80.10.30");	  strcpy(ip_matrix[3].matx_nameof_ip,"NetExtender_VPN");
+	ip_matrix[3].matx_chproc_num = 3;  strcpy(ip_matrix[3].matx_ip,"10.80.10.30");    strcpy(ip_matrix[3].matx_nameof_ip,"NetExtender_VPN");
 	ip_matrix[4].matx_chproc_num = 4;  strcpy(ip_matrix[4].matx_ip,"10.80.244.125");  strcpy(ip_matrix[4].matx_nameof_ip,"Office_Ethernet");
 
 	if(argc!=2)
@@ -48,17 +48,18 @@ void main(int argc, char *argv[])
 		return;
 	}
 
-	if(strcmp(argv[0], argv[1]) != 0)	// check if used cmd is $ sendtoLap sendtoLap , if yes means file to be sent is edited manually
+	if(strcmp(argv[0], argv[1]) == 0)	// check if used cmd is $ sendtoLap sendtoLap , if yes means file to be sent is edited manually
 						// in this case, that edit file directly should be sent, not content passed on CLI
+	{
+		printf("\n Received cmd: $ sendtoLap sendtoLap. Sending manually edited file ~/.sk/.sk_gitignore/send-THIS2Lap.txt \n");
+	}
+	else 					// else if(strcmp(argv[0], argv[1]) != 0) - Used cmd is $ sendtoLap NOTEQUAL to "sendtoLap" string
 	{
 		FILE *fw = fopen("/home/shubham/.sk/.sk_gitignore/send-THIS2Lap.txt", "w");
 		fprintf(fw, "%s\n", argv[1]);
 		fclose(fw);
 	}
-	else
-	{
-		printf("\n Received cmd: $ sendtoLap sendtoLap. Sending manually edited file ~/.sk/.sk_gitignore/send-THIS2Lap.txt \n");
-	}
+
 
 /////////////
 
@@ -66,10 +67,11 @@ void main(int argc, char *argv[])
 
 	if( (chproc_num++, pid_arr[1]=fork()) && (chproc_num++, pid_arr[2]=fork()) && \
 	    (chproc_num++, pid_arr[3]=fork()) && (chproc_num++, pid_arr[4]=fork()) )
-	{
+	{	// Will be executed once by Parent main() process
+	
 		chproc_num=0;		// for Parent, set chproc_num i.e. child_process_number = 0
 
-		printf("in pid=%d parent, childproc_num=%d\n", pid_arr[0], chproc_num);
+		printf("fork() %d times. main() parent_pid=%d.\n", NUMBER_OF_IPs, pid_arr[0]);
 #ifdef debug
 		for(int j=1; j<=ips_cnt; j++)
 		{
@@ -82,7 +84,7 @@ void main(int argc, char *argv[])
 //		printf(" \t\t KEEP this pid=%d \n", first_ret_pid);
 		for(int j=1; j<=ips_cnt; j++)
 		{
-			if(pid_arr[j] != first_ret_pid)	// KILL these PID
+			if(pid_arr[j] != first_ret_pid)	// KILL these PIDs
 			{
 				sprintf(pid_str, "%d", pid_arr[j]);
 				strcpy(buffer_pid_killer, "kill -9 ");	strcat(buffer_pid_killer, pid_str);
@@ -95,7 +97,9 @@ void main(int argc, char *argv[])
 				ret_chproc_num=j;
 			}
 		}
-		printf(" \t\t KEEP this pid=%d [%s]\n", first_ret_pid, ip_matrix[ret_chproc_num].matx_nameof_ip);
+		printf("\n");
+		printf(" \t\t KEEP THIS    pid=%d [%s]\n", first_ret_pid, ip_matrix[ret_chproc_num].matx_nameof_ip);
+		printf("\n");
 
 		for(int j=1; j<=ips_cnt-1; j++)		// '-1' was done as already once wait(0) used to capture first_ret_pid
 		{
@@ -125,31 +129,37 @@ void main(int argc, char *argv[])
 #ifdef debug
 		printf("inside parent at end.. \n");
 #endif
-	}
-	else			      //		 10	   20   25   30     37
-	{			      //		 10	   20   25   30     37
-				      //0123456789        0123456789012345678901234567
-		strcpy(buffer_scp_cmd, "ping -c 1 ");	//xyz.xyz.xyz.xyz > /dev/null
+	}// if() for parent ends
+	
+	else // Will be Executed by 4xfork()ed Child Processes
+	{			      //		 10	   20   25   30     37		// Reference for "strncpy(buffer_scp_cmd+..." command proper output for all possible IPs
+				      //		 10	   20   25   30     37		// Reference for "strncpy(buffer_scp_cmd+..." command proper output for all possible IPs
+				      //0123456789        0123456789012345678901234567	// Reference for "strncpy(buffer_scp_cmd+..." command proper output for all possible IPs
+		strcpy(buffer_scp_cmd, "ping -c 1 ");	   //xyz.xyz.xyz.xyz > /dev/null
 		strcpy(buffer_scp_cmd+25, " > /dev/null");
 
-	char tmp_cp_buff[24]={0};
+		char tmp_cp_buff[24]={0};
 
 		switch (chproc_num)
 		{
-		case 1: strcpy(tmp_cp_buff, ip_matrix[1].matx_ip);	for(int j=strlen(tmp_cp_buff); j<23; j++){tmp_cp_buff[j]=' ';}
-			strncpy(buffer_scp_cmd+10, tmp_cp_buff, 15);	//printf("_%s_\n", buffer_scp_cmd);
+		case 1: 						strcpy(tmp_cp_buff, ip_matrix[1].matx_ip);	for(int j=strlen(tmp_cp_buff); j<23; j++){tmp_cp_buff[j]=' ';}
+			strncpy(buffer_scp_cmd+10, tmp_cp_buff, 15);						//printf("_%s_\n", buffer_scp_cmd);
+
 			if( system(buffer_scp_cmd) != 0) {printf("\n\t Please check the IP Matrix.\t FAILED \t to connect any IP \n\n\n");} break;
 
-		case 2: strcpy(tmp_cp_buff, ip_matrix[2].matx_ip);	for(int j=strlen(tmp_cp_buff); j<23; j++){tmp_cp_buff[j]=' ';}
-			strncpy(buffer_scp_cmd+10, tmp_cp_buff, 15);  	//printf("_%s_\n", buffer_scp_cmd);
+		case 2: 						strcpy(tmp_cp_buff, ip_matrix[2].matx_ip);	for(int j=strlen(tmp_cp_buff); j<23; j++){tmp_cp_buff[j]=' ';}
+			strncpy(buffer_scp_cmd+10, tmp_cp_buff, 15);  					//printf("_%s_\n", buffer_scp_cmd);
+
 			if( system(buffer_scp_cmd) != 0) {printf("\n\t Please check the IP Matrix.\t FAILED \t to connect any IP \n\n\n");} break;
 
-		case 3: strcpy(tmp_cp_buff, ip_matrix[3].matx_ip);	for(int j=strlen(tmp_cp_buff); j<23; j++){tmp_cp_buff[j]=' ';}
-			strncpy(buffer_scp_cmd+10, tmp_cp_buff, 15);  	//printf("_%s_\n", buffer_scp_cmd);
+		case 3: 						strcpy(tmp_cp_buff, ip_matrix[3].matx_ip);	for(int j=strlen(tmp_cp_buff); j<23; j++){tmp_cp_buff[j]=' ';}
+			strncpy(buffer_scp_cmd+10, tmp_cp_buff, 15);  					//printf("_%s_\n", buffer_scp_cmd);
+
 			if( system(buffer_scp_cmd) != 0) {printf("\n\t Please check the IP Matrix.\t FAILED \t to connect any IP \n\n\n");} break;
 
-		case 4: strcpy(tmp_cp_buff, ip_matrix[4].matx_ip);	for(int j=strlen(tmp_cp_buff); j<23; j++){tmp_cp_buff[j]=' ';}
-			strncpy(buffer_scp_cmd+10, tmp_cp_buff, 15);  	//printf("_%s_\n", buffer_scp_cmd);
+		case 4: 						strcpy(tmp_cp_buff, ip_matrix[4].matx_ip);	for(int j=strlen(tmp_cp_buff); j<23; j++){tmp_cp_buff[j]=' ';}
+			strncpy(buffer_scp_cmd+10, tmp_cp_buff, 15);  					//printf("_%s_\n", buffer_scp_cmd);
+
 			if( system(buffer_scp_cmd) != 0) {printf("\n\t Please check the IP Matrix.\t FAILED \t to connect any IP \n\n\n");} break;
 		}
 	}
