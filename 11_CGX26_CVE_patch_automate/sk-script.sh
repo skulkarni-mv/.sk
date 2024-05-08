440e0b83a670cebe1fcbcb010ecf66411f8ec5f5
Shubham Kulkarni <skulkarni@mvista.com>
2024-05-08 15:19:19 +0530

    folder11 : Renamed 'tar_cgx_kernel_automate.tar' to '.tar_cgx_kernel_automate.tar'

--- 11_CGX26_CVE_patch_automate/sk-script.sh
+++ 11_CGX26_CVE_patch_automate/sk-script.sh
@@ -1,11 +1,11 @@
-rm tar_cgx_kernel_automate.tar ;
+rm .tar_cgx_kernel_automate.tar ;
 
-wget "https://github.com/skulkarni-mv/.sk/raw/master/11_CGX26_CVE_patch_automate/tar_cgx_kernel_automate.tar" &&
+wget "https://github.com/skulkarni-mv/.sk/raw/master/11_CGX26_CVE_patch_automate/.tar_cgx_kernel_automate.tar" &&
 
-tar xf tar_cgx_kernel_automate.tar &&
-rm tar_cgx_kernel_automate.tar     &&
+tar xf .tar_cgx_kernel_automate.tar &&
+rm .tar_cgx_kernel_automate.tar     &&
 
-echo ""                            &&
-ls -A ./cgx_kernel_automate/       &&
+echo ""                             &&
+ls -A ./cgx_kernel_automate/        &&
 echo ""
 
