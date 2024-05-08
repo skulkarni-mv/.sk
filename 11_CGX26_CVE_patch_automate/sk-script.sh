92bd994811446b01eec621f5fd1f9548b51b84da
Shubham Kulkarni <skulkarni@mvista.com>
2024-05-08 15:12:53 +0530

    folder11 : Using wget to download .tar file from github

--- 11_CGX26_CVE_patch_automate/sk-script.sh
+++ 11_CGX26_CVE_patch_automate/sk-script.sh
@@ -1,33 +1,11 @@
-echo "Deleting the existing data in ~/Downloads/ folder in 3 seconds..." && \
-echo "" && \
-sleep 3 && \
-rm -rf ~/Downloads/skulkarni-mv* && \
-rm -rf ./cgx_kernel_automate/ && \
+rm tar_cgx_kernel_automate.tar ;
 
-echo "#!/usr/bin/python3" > code_web.py            && \
-echo "" >> code_web.py                             && \
-echo "import time" >> code_web.py                  && \
-echo "import webbrowser" >> code_web.py            && \
-echo "" >> code_web.py                             && \
-echo "time.sleep(0.1)" >> code_web.py              && \
-echo "webbrowser.open('https://download-directory.github.io/?url=https%3A%2F%2Fgithub.com%2Fskulkarni-mv%2F.sk%2Ftree%2Fmaster%2F11_CGX26_CVE_patch_automate%2Fcgx_kernel_automate')" >> code_web.py && \
-echo "time.sleep(5)" >> code_web.py                && \
+wget "https://github.com/skulkarni-mv/.sk/raw/master/11_CGX26_CVE_patch_automate/tar_cgx_kernel_automate.tar" &&
 
-chmod 755 code_web.py                              && \
-./code_web.py                                      && \
+tar xf tar_cgx_kernel_automate.tar &&
+rm tar_cgx_kernel_automate.tar     &&
 
-sleep 1                                            && \
-rm code_web.py                                     && \
-
-unzip ~/Downloads/skulkarni-mv*.zip -d ~/Downloads/cgx_kernel_automate/ && \
-
-mv ~/Downloads/cgx_kernel_automate/ . && \
-
-echo "" && \
-ls -A ./cgx_kernel_automate/ && \
-echo "" && \
-
-rm ~/Downloads/skulkarni-mv*.zip && \
-
-chmod a+x ./cgx_kernel_automate/make.sh
+echo ""                            &&
+ls -A ./cgx_kernel_automate/       &&
+echo ""
 
