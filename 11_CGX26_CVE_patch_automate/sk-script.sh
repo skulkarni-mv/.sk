8c8a8f2e61cbd64490a2929c94606aac3c26b469
Shubham Kulkarni <skulkarni@mvista.com>
2024-05-08 17:39:28 +0530

    folder11 : Updated 'sk-script.sh' to show 'ReadMe.txt' reference

--- 11_CGX26_CVE_patch_automate/sk-script.sh
+++ 11_CGX26_CVE_patch_automate/sk-script.sh
@@ -7,5 +7,8 @@ rm .tar_cgx_kernel_automate.tar     &&
 
 echo ""                             &&
 ls -A ./cgx_kernel_automate/        &&
+echo ""                             &&
+
+echo "Downloaded folder \"cgx_kernel_automate\". Check \"ReadMe.txt\" file inside for details and run \"make.sh\" script to start" &&
 echo ""
 
