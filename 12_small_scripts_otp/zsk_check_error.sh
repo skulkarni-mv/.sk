ffe8fc7134a74044b10eb4d0282ef97fb14317a4
Shubham Kulkarni <skulkarni@mvista.com>
2024-12-03 16:12:07 +0530

    folder12: Update code in 'zsk_check_error.sh'

--- 12_small_scripts_otp/zsk_check_error.sh
+++ 12_small_scripts_otp/zsk_check_error.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 
-if [ $1 != '' ]
+if [ -n "$1" ] && [ -e "$1" ]
 then
 
-  echo "shubham=0; {"  >> $1 && echo "" && echo "" && git diff $1 && echo "" && echo "" && make bzImage -j8 > kbuild.log ; echo "" ; git reset --hard ; echo ""
+  echo "shubham=0; {"  >> $1 && echo "" && echo "" && git diff $1 && echo "" && echo "" && make bzImage -j8 ; echo "" ; git reset --hard ; echo ""
 
+else
+  echo""; echo "ERROR: Please provide CORRECT 'source code / .c' file name OR file which EXISTS."; echo ""
 fi
 
 
